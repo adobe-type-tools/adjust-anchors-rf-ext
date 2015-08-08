ef7fe9b24030c7e7cc64bd97e20d8d9c3069ab19
Miguel Sousa <msousa@adobe.com>
2015-08-08 02:56:53 -0700

    white space

--- AdjustAnchors.roboFontExt/lib/AdjustAnchors.py
+++ AdjustAnchors.roboFontExt/lib/AdjustAnchors.py
@@ -1,6 +1,6 @@
 """
 Copyright 2015 Adobe Systems Incorporated. All rights reserved.
- 
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -41,7 +41,7 @@ extensionName = "Adjust Anchors"
 
 
 class AdjustAnchors(BaseWindowController):
-	
+
 	def __init__(self):
 		self.font = CurrentFont()
 		self.glyph = CurrentGlyph()
@@ -52,9 +52,9 @@ class AdjustAnchors(BaseWindowController):
 		self.fillAnchorsAndMarksDicts()
 		self.glyphNamesList = [] # list of glyph names that will be displayed in the UI list
 		self.selectedGlyphNamesList = [] # list of glyph names selected in the UI list
-		
+
 		self.Blue, self.Alpha = 1, 0.6
-		
+
 		self.font.naked().addObserver(self, "fontWasModified", "Font.Changed")
 		addObserver(self, "_fontWillClose", "fontWillClose")
 		addObserver(self, "_currentFontChanged", "fontResignCurrent")
@@ -70,11 +70,11 @@ class AdjustAnchors(BaseWindowController):
 		textSizeNumFormatter.setAllowsFloats_(False)
 		textSizeNumFormatter.setGeneratesDecimalNumbers_(False)
 		textSizeNumFormatter.setMinimum_(NSNumber.numberWithInt_(1))
-		
+
 		lineHeightNumFormatter = NSNumberFormatter.alloc().init()
 		lineHeightNumFormatter.setAllowsFloats_(False)
 		lineHeightNumFormatter.setGeneratesDecimalNumbers_(False)
-		
+
 		self.textSize = getExtensionDefault("%s.%s" % (extensionKey, "textSize"))
 		if not self.textSize:
 			self.textSize = 150
@@ -86,12 +86,12 @@ class AdjustAnchors(BaseWindowController):
 		posSize = getExtensionDefault("%s.%s" % (extensionKey, "posSize"))
 		if not posSize:
 			posSize = (100, 100, 1200, 400)
-		
+
 		self.w = FloatingWindow(posSize, extensionName, minSize=(500, 400))
 		self.w.fontList = List((10, 10, 190, -10), self.glyphNamesList, selectionCallback = self.listSelectionCallback)
-		self.w.lineView = MultiLineView((210, 10, -10, -41), 
-							pointSize = self.textSize, 
-							lineHeight = self.lineHeight, 
+		self.w.lineView = MultiLineView((210, 10, -10, -41),
+							pointSize = self.textSize,
+							lineHeight = self.lineHeight,
 							displayOptions={"Beam" : False, "displayMode" : "Multi Line"}
 							)
 		self.w.textSizeLabel = TextBox((210, -30, 100, -10), "Text Size")
@@ -104,8 +104,8 @@ class AdjustAnchors(BaseWindowController):
 
 		self.w.bind("close", self.windowClose)
 		self.w.open()
-		
-	
+
+
 	def textSizeCallback(self, sender):
 		try: # in case the user submits an empty field
 			self.textSize = int(sender.get())
@@ -113,8 +113,8 @@ class AdjustAnchors(BaseWindowController):
 			NSBeep()
 			self.w.textSize.set(self.textSize)
 		self.w.lineView.setPointSize(self.textSize)
-		
-	
+
+
 	def lineHeightCallback(self, sender):
 		try:
 			self.lineHeight = int(sender.get())
@@ -122,8 +122,8 @@ class AdjustAnchors(BaseWindowController):
 			NSBeep()
 			self.w.lineHeight.set(self.lineHeight)
 		self.w.lineView.setLineHeight(self.lineHeight)
-		
-	
+
+
 	def windowClose(self, sender):
 		self.font.naked().removeObserver(self, "Font.Changed")
 		removeObserver(self, "fontWillClose")
@@ -139,16 +139,16 @@ class AdjustAnchors(BaseWindowController):
 		setExtensionDefault("%s.%s" % (extensionKey, "posSize"), self.w.getPosSize())
 		setExtensionDefault("%s.%s" % (extensionKey, "textSize"), self.textSize)
 		setExtensionDefault("%s.%s" % (extensionKey, "lineHeight"), self.lineHeight)
-	
-	
+
+
 	def _previewFill(self, info):
 		self.Blue, self.Alpha = 0, 1
 
 
 	def _drawFill(self, info):
 		self.Blue, self.Alpha = 1, 0.6
-	
-	
+
+
 	def _fontWillClose(self, info):
 		"""
 			Close the window when the last font is closed
@@ -156,8 +156,8 @@ class AdjustAnchors(BaseWindowController):
 		if len(AllFonts()) < 2:
 			self.windowClose(self)
 			self.w.close()
-	
-	
+
+
 	def _currentFontChanged(self, info):
 		self.font.naked().removeObserver(self, "Font.Changed")
 		self.font = CurrentFont()
@@ -166,12 +166,12 @@ class AdjustAnchors(BaseWindowController):
 		self.glyphNamesList = []
 		self.selectedGlyphNamesList = []
 		self.updateExtensionWindow()
-	
-	
+
+
 	def _currentGlyphChanged(self, info):
 		self.updateExtensionWindow()
-	
-	
+
+
 	def fontWasModified(self, info):
 		OutputWindow().clear()
 		self.fillAnchorsAndMarksDicts()
@@ -186,7 +186,7 @@ class AdjustAnchors(BaseWindowController):
 		else:
 			for component in gToAppend.components:
 				compGlyph = self.font[component.baseGlyph].copy()
-				
+
 				# handle component transformations
 				componentTransformation = component.transformation
 				# when undoing a paste anchor or a delete anchor action, RoboFont returns component.transformation as a list instead of a tuple
@@ -197,7 +197,7 @@ class AdjustAnchors(BaseWindowController):
 					if matrix != (1, 0, 0, 1): # if component is skewed
 						transformObj = Identity.transform(matrix + (0, 0)) # ignore the original component's shifting values
 						compGlyph.transform(transformObj)
-				
+
 				glyph.appendGlyph(compGlyph, map(sum, zip(component.offset, offset))) # add the two tuples of offset
 			for contour in gToAppend:
 				glyph.appendContour(contour, offset)
@@ -207,10 +207,10 @@ class AdjustAnchors(BaseWindowController):
 			nestedComponent = glyph.components[-1] # start from the end
 			glyph.removeComponent(nestedComponent)
 			glyph = self.deepAppendGlyph(glyph, self.font[nestedComponent.baseGlyph], nestedComponent.offset)
-			
+
 		return glyph
-	
-	
+
+
 	def updateExtensionWindow(self):
 		if CurrentGlyph() is not None: # NOTE: CurrentGlyph() will return zero (its length), so "is not None" is necessary
 			self.glyph = CurrentGlyph()
@@ -218,39 +218,39 @@ class AdjustAnchors(BaseWindowController):
 			self.updateListView()
 			currentGlyphName = self.glyph.name
 			upm = self.font.info.unitsPerEm
-		
+
 			# base glyph + accent combinations preview
 			# first check if there's a cached glyph
 			if currentGlyphName in self.glyphPreviewCacheDict:
 				self.w.lineView.set(self.glyphPreviewCacheDict[currentGlyphName])
-			
+
 			# assemble the glyphs
 			else:
 				glyphsList = []
 				for glyphNameInUIList in self.glyphNamesList:
 					newGlyph = RGlyph()
 					newGlyph.setParent(self.font)
-					
+
 					# the glyph in the UI list is a mark
 					if glyphNameInUIList in self.marksDict:
 						markGlyph = self.font[glyphNameInUIList]
-			
+
 						# append base glyph
 						newGlyph = self.deepAppendGlyph(newGlyph, self.glyph)
 						# append mark glyph
 						newGlyph = self.deepAppendGlyph(newGlyph, markGlyph, self.getAnchorOffsets(self.glyph, markGlyph))
-					
+
 						# set the advanced width
 						if self.glyph.width < 10: # combining marks or other glyphs with a small advanced width
 							newGlyph.leftMargin = upm * .05 # 5% of the UPM
 							newGlyph.rightMargin = newGlyph.leftMargin
 						else:
 							newGlyph.width = self.glyph.width
-						
+
 					# the glyph in the UI list is a base
 					else:
 						baseGlyph = self.font[glyphNameInUIList]
-			
+
 						# append base glyph
 						newGlyph = self.deepAppendGlyph(newGlyph, baseGlyph)
 						# append mark glyph
@@ -262,24 +262,24 @@ class AdjustAnchors(BaseWindowController):
 							newGlyph.rightMargin = newGlyph.leftMargin
 						else:
 							newGlyph.width = baseGlyph.width
-			
+
 					# pad the new glyph if it has too much overhang
 					if newGlyph.leftMargin < upm * .15:
 						newGlyph.leftMargin = upm * .05
 					if newGlyph.rightMargin < upm * .15:
 						newGlyph.rightMargin = upm * .05
-		
+
 					# one last check for making sure the new glyph can be displayed
 					if not newGlyph.components:
 						glyphsList.append(newGlyph)
 					else:
 						print "Combination with mark glyph %s can't be previewed because it contains component %s." % (glyphNameInUIList, newGlyph.components[0].baseGlyph)
-				
+
 				self.w.lineView.set(glyphsList)
-		
+
 				# add to the cache
 				self.glyphPreviewCacheDict[currentGlyphName] = glyphsList
-		
+
 
 	def listSelectionCallback(self, sender):
 		selectedGlyphNamesList = []
@@ -287,13 +287,13 @@ class AdjustAnchors(BaseWindowController):
 			selectedGlyphNamesList.append(self.glyphNamesList[index])
 		self.selectedGlyphNamesList = selectedGlyphNamesList
 		self.updateGlyphView()
-		
-	
+
+
 	def updateGlyphView(self):
 		# update the current glyph view
 		UpdateCurrentGlyphView()
-	
-	
+
+
 	def fillAnchorsAndMarksDicts(self):
 		# reset all the dicts
 		self.glyphPreviewCacheDict = {}
@@ -301,7 +301,7 @@ class AdjustAnchors(BaseWindowController):
 		self.anchorsOnBasesDict = {}
 		self.marksDict = {}
 		markGlyphsWithMoreThanOneAnchorTypeList = []
-		
+
 		for glyphName in self.font.glyphOrder:
 			glyphAnchorsList = self.font[glyphName].anchors
 			for anchor in glyphAnchorsList:
@@ -329,12 +329,12 @@ class AdjustAnchors(BaseWindowController):
 						tempList = self.anchorsOnBasesDict[anchorName]
 						tempList.append(glyphName)
 						self.anchorsOnBasesDict[anchorName] = tempList
-		
+
 		if markGlyphsWithMoreThanOneAnchorTypeList:
 			for glyphName in markGlyphsWithMoreThanOneAnchorTypeList:
 				print "ERROR: Glyph %s has more than one type of anchor." % glyphName
-			
-	
+
+
 	def makeGlyphNamesList(self, glyph):
 		glyphNamesList = []
 		markGlyphIsAbleToBeBase = False
@@ -346,7 +346,7 @@ class AdjustAnchors(BaseWindowController):
 					glyphNamesList.extend(self.anchorsOnMarksDict[anchorName])
 				elif anchorName[1:] in self.anchorsOnBasesDict: # skips the leading underscore
 					glyphNamesList.extend(self.anchorsOnBasesDict[anchorName[1:]])
-		
+
 			# for mark glyphs, test if they're able to get other mark glyphs attached to them
 			# this will (correctly) prevent the UI list from including glyph names that cannot be displayed with the current glyph
 			if glyph.name in self.marksDict:
@@ -359,10 +359,10 @@ class AdjustAnchors(BaseWindowController):
 					for glyphName in glyphNamesList[::-1]: # iterate from the end of the list
 						if glyphName in self.marksDict:
 							glyphNamesList.remove(glyphName)
-		
+
 		return glyphNamesList
 
-	
+
 	def updateListView(self):
 		self.w.fontList.set(self.glyphNamesList)
 
@@ -396,7 +396,7 @@ class AdjustAnchors(BaseWindowController):
 					if anchor.name == anchorName:
 						baseAnchor = anchor
 						break
-			
+
 			try:
 				offsetX = markAnchor.x - baseAnchor.x
 				offsetY = markAnchor.y - baseAnchor.y
@@ -407,7 +407,7 @@ class AdjustAnchors(BaseWindowController):
 		# the current glyph is a base
 		else:
 			anchorName = self.marksDict[glyphToDraw.name]
-		
+
 			# pick the (base glyph) anchor to draw on
 			for anchor in canvasGlyph.anchors:
 				if anchor.name == anchorName:
@@ -425,30 +425,30 @@ class AdjustAnchors(BaseWindowController):
 			except UnboundLocalError:
 				offsetX = 0
 				offsetY = 0
-	
+
 		return (offsetX, offsetY)
 
 
 	def _drawGlyphs(self, info):
 		""" draw stuff in the glyph window view """
 		translateBefore = (0, 0)
-		
+
 		for glyphName in self.selectedGlyphNamesList:
 			glyphToDraw = self.font[glyphName]
-			
+
 			# determine the offset of the anchors
 			offset = self.getAnchorOffsets(self.glyph, glyphToDraw)
-			
+
 			# set the offset of the drawing
 			translate(offset[0] - translateBefore[0], offset[1] - translateBefore[1])
-	
+
 			# record the shift amounts (these are needed for resetting the drawing position when more than one mark is selected on the list)
 			translateBefore = offset
 
 			# set the fill & stroke
 			fill(0, 0, self.Blue, self.Alpha)
 			strokeWidth(None)
-	
+
 			# draw it
 			mojoPen = MojoDrawingToolsPen(glyphToDraw, self.font)
 			glyphToDraw.draw(mojoPen)
