dd4d4b39794aed5427f4f4018c9ecdab3237246f
Miguel Sousa <msousa@adobe.com>
2017-05-03 13:29:43 -0700

    Bump version number

--- AdjustAnchors.roboFontExt/lib/AdjustAnchors.py
+++ AdjustAnchors.roboFontExt/lib/AdjustAnchors.py
@@ -1,5 +1,5 @@
 """
-Copyright 2015-2016 Adobe Systems Incorporated. All rights reserved.
+Copyright 2015-2017 Adobe Systems Incorporated. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
